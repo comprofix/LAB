f5aff009d5f8da20cb1e09ad41e8d4e6f3f37b86
Matthew McKinnon <mmckinnon@comprofix.com>
2025-07-31 16:58:48 +1000

    chore: add kms script

---
+++ kms.ps1
@@ -0,0 +1,21 @@
+param(
+    [string]$Server,
+    [string]$Version
+    
+)
+
+switch ($Version) {
+    "2019" { $key = "N69G4-B89J2-4G8F4-WWYCC-J464C" }
+    "2022" { $key = "VDYBN-27WPP-V4HQT-9VMD4-VMK7H" }
+    default { Write-Host "❌ [ERROR] - Unknown server"; exit 1 }
+}
+
+cscript //nologo c:\windows\system32\slmgr.svb /upk
+cscript //nologo c:\windows\system32\slmgr.svb /ipk $Key
+cscript //nologo c:\windows\system32\slmgr.svb /ipk $Key
+cscript //nologo c:\windows\system32\slmgr.svb /skms $server:1688
+cscript //nologo c:\windows\system32\slmgr.svb /ato
+
+
+
+
