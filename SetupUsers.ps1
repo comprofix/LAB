6ba7b4d5ddb82e622e3e0a6af34333a84280311a
Matthew McKinnon <mmckinnon@comprofix.com>
2025-08-01 18:31:59 +1000

    chore: add setup users

---
+++ SetupUsers.ps1
@@ -0,0 +1,30 @@
+param(
+    [string]$Domain,
+    [string]$OU
+
+)
+
+
+$Users = Import-Csv .\Users.csv
+
+ForEach ($User in $Users) {
+    $DisplayName = $User.DisplayName
+    $FirstName = $User.FirstName
+    $Surname = $User.Surname
+    $SurnameInitial = $User.Surname[0]
+
+    Write-Host $DisplayName "$FirstName$SurnameInitial@$Domain"
+
+    Write-Host "Creating sample users..." -ForegroundColor Cyan
+    $UserPassword = ConvertTo-SecureString "P@ssw0rd123!" -AsPlainText -Force
+
+    New-ADUser -Name "$DisplayName" -GivenName "$FirstName" -Surname "$Surname" `
+        -SamAccountName "$FirstName$SurnameInitial" -UserPrincipalName "$FirstName$SurnameInitial@$Domain" `
+        -AccountPassword $UserPassword -Enabled $true `
+        -Path "$OU" -ErrorAction SilentlyContinue
+
+    Set-ADUser -Identity "$FirstName$SurnameInitial" -EmailAddress "$FirstName$SurnameInitial@$Domain"
+    Set-ADUser -Identity "$FirstName$SurnameInitial" -Add @{proxyAddresses="SMTP:$FirstName$SurnameInitial@$Domain"}
+    
+
+}
