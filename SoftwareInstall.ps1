678fccc2e7eb169038f1f6c9fd46173fe56f83ba
Matthew McKinnon <mmckinnon@comprofix.com>
2026-03-01 06:21:47 +1000

    chore: add winget

--- SoftwareInstall.ps1
+++ SoftwareInstall.ps1
@@ -32,8 +32,6 @@ $packages = @{
     'nerd-fonts-hack'               = ''
     'cascadiamono'                  = ''
     'powershell-core'               = ''
-    'microsoft-windows-terminal'    = ''
-    'winget-cli'                    = ''
 }
 
 # Loop through packages
@@ -55,6 +53,20 @@ try {
 } catch {
     Write-Output "❌ - Failed to upgrade packages $_"
 }
+
+# Installing Winget
+Start-BitsTransfer -Source "https://github.com/microsoft/winget-cli/releases/latest/download/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" -Destination "~\Downloads\WinGet.msixbundle"
+Start-BitsTransfer -Source "https://github.com/microsoft/winget-cli/releases/latest/download/DesktopAppInstaller_Dependencies.zip" -Destination "~\Downloads\DesktopAppInstaller_Dependencies.zip"
+Start-BitsTransfer -Source "https://github.com/microsoft/winget-cli/releases/latest/download/e53e159d00e04f729cc2180cffd1c02e_License1.xml" -Destination "~\Downloads\license.xml"
+Expand-Archive -Path "~\Downloads\DesktopAppInstaller_Dependencies.zip"
+Add-AppxPackage "~\Downloads\DesktopAppInstaller_Dependencies\x64\Microsoft.UI.Xaml*x64.appx"
+Add-AppxPackage "~\Downloads\DesktopAppInstaller_Dependencies\x64\Microsoft.VCLibs*x64.appx"
+Add-AppxPackage "~\Downloads\DesktopAppInstaller_Dependencies\x64\Microsoft.WindowsAppRuntime*x64.appx"
+Add-AppxProvisionedPackage -Online -PackagePath "~\Downloads\WinGet.msixbundle" -LicensePath .\license.xml
+Get-AppPackage *Microsoft.DesktopAppInstaller*|select Name,PackageFullName
+winget --info
+Remove-Item -Path "~\Downloads\WinGet.msixbundle", "~\Downloads\DesktopAppInstaller_Dependencies.zip", "~\Downloads\DesktopAppInstaller_Dependencies", "~\Downloads\license.xml" -Recurse -Force
+
 
 $env:PATH = "C:\Program Files\Git\cmd;" + $env:PATH
 Write-Output "✅ - Update Paths"
